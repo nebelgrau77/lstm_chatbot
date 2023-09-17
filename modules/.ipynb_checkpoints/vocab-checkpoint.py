6fd597702d3208d5521e16527a83c5b8aba68f04
u076079 <michal.lasak@stellantis.com>
2023-09-17 18:16:39 +0000

    added word removal from vocabulary and dataframe cleanup if contains words removed

--- modules/.ipynb_checkpoints/vocab-checkpoint.py
+++ modules/.ipynb_checkpoints/vocab-checkpoint.py
@@ -2,16 +2,16 @@
 PAD_token = 0
 SOS_token = 1
 EOS_token = 2
-
-NO UNK TOKEN!
+UNK_token = 3
 '''
 
 class Vocab:
     def __init__(self):        
-        self.word2index = {"PAD":0, "SOS":1, "EOS":2}        
-        self.index2word = {0: "PAD", 1: "SOS", 2: "EOS"}
-        self.word2count = {}
-        self.n_words = len(self.word2index) # count PAD, SOS and EOS tokens
+        self.word2index = {"<PAD>":0, "<SOS>":1, "<EOS>":2, "<UNK>":3}        
+        self.index2word = {0: "<PAD>", 1: "<SOS>", 2: "<EOS>", 3: "<UNK>"}
+        # make sure that the special tokens don't get removed as too rare!
+        self.word2count = {"<PAD>":9999999, "<SOS>":9999999, "<EOS>":9999999, "<UNK>":9999999, }
+        self.n_words = len(self.word2index) # count PAD, SOS, EOS and UNK tokens
         
     def add_sentence(self, sentence):
         for word in sentence:
@@ -27,3 +27,12 @@ class Vocab:
             self.word2count[word] += 1
 
 
+    def remove_word(self, word):
+        idx = self.word2index[word]        
+        self.word2index.pop(word)
+        self.index2word.pop(idx)
+        self.word2count.pop(word)
+        self.n_words -= 1
+        
+
+    
